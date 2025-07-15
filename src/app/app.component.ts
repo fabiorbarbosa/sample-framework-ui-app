1d8caadb748b1dfefde159221a4a40658f7f86f6
Fábio Barbosa <fabiobarbosaa@gmail.com>
2025-07-15 18:40:53 -0300

    feat: add reference

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ButtonComponent } from '@sample-framework-ui/button';
+import { CardComponent } from '@sample-framework-ui/card';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, ButtonComponent, CardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
