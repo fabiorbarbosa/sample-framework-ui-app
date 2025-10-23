a587eaeff1b7c620a58888738214aa55d0abc3c5
Fábio Barbosa <fabiobarbosaa@gmail.com>
2025-10-23 15:37:35 -0300

    feat: update reference

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ButtonComponent } from '@sample-framework-ui/button';
 import { CardComponent } from '@sample-framework-ui/card';
+import { InputComponent } from '@sample-framework-ui/input';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ButtonComponent, CardComponent],
+  imports: [RouterOutlet, ButtonComponent, CardComponent, InputComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
